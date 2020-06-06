ff50c37e2ae1d8ba737e9b3c761c5ae2cb5719a3
joshkim999 <63277870+joshkim999@users.noreply.github.com>
2020-06-06 15:57:48 -0700

    Update assign_apikey.R

--- R/assign_apikey.R
+++ R/assign_apikey.R
@@ -1,7 +1,7 @@
 #'
 #' Allows you to use the inputted api key (from alphavantage) for this package
 #'
-#' @param apikey Your apikey from alphavantage
+#' @param apikey Your apikey from alphavantage as a String
 #'
 #'
 #' @return NULL, but the apikey is saved for the usage of the package
